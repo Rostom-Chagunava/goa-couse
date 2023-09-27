21dd8dd17cbd04a1e4ec00cbcfdd7826e9becaf8
Rostom-Chagunava <145435217+Rostom-Chagunava@users.noreply.github.com>
2023-09-27 13:32:10 +0200

    Add files via upload

---
+++ classwork/lessen1.py
@@ -0,0 +1,11 @@
+print("Knowledge " + "is " + "power")
+
+
+print("Spider", "Man", sep = "-")
+
+num = 4
+print(num*5)
+
+speed=550 
+distance=7425
+print(distance/speed)
---
+++ homework/lessen1.py
@@ -0,0 +1,7 @@
+mom_age=50
+ded_age=65
+my_age=29
+
+print(mom_age+5)
+print(ded_age+5)
+print(my_age+5)
