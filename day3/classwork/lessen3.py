4dfba10ca17cbffec421325c69f3393eff516903
Rostom-Chagunava <145435217+Rostom-Chagunava@users.noreply.github.com>
2023-09-30 10:32:41 +0200

    Add files via upload

---
+++ day3/classwork/lessen3.py
@@ -0,0 +1,11 @@
+
+# name=input("your name:")
+# surname=input("your surname:")
+# age=input("your age:")
+# print(name + surname)
+#print(name +" "+ surname+ " "+age)
+
+
+age=int(input("your age:"))
+age=age+25
+print(age)
