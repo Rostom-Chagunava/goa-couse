3b83d344b940e1395e3fd65e9c27389d955b931f
Rostom-Chagunava <145435217+Rostom-Chagunava@users.noreply.github.com>
2023-10-07 19:47:57 +0200

    Add files via upload

---
+++ day5/classwork/lesson5.py
@@ -0,0 +1,9 @@
+name=input("your name:")
+age=int(input("your age:"))
+years=2024
+for i in range(10): 
+ print("hi"+" "+name+" "+" შენ იქნები"+" "+ str(years+i)+" "+"წლის"+str(age+1+i)+" "+"წლის")
+seats=10
+while seats >0:
+    print("Seal ticket"+str(seats))
+    seats=seats-1
---
+++ day5/homeworh/lesson5.py
@@ -0,0 +1,20 @@
+# your_name=input("your name:")
+# your_age=int(input("your age:"))
+# father_age=int(input("father age:"))
+# year=2023
+
+# for i in range(8):
+#     print("შენ იქნები "+str(year+i) +" წელს "+str(( father_age//your_age))+" ჯერ "+" უნცროსი "+" მამაზე ")
+numer=2
+while numer <31:
+    print(" ლ უ წ ი:"+ str(numer))
+    numer+=2
+numer_1=1 
+print("===")    
+while numer_1 <30:
+    print(" კ ე ნ ტ ი : "+str(numer_1))    
+    numer_1+=1
+   
+    
+
+    
