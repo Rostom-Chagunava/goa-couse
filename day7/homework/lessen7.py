d78fb5a81dc48d1bfb70577c2dff49860cea009f
Rostom-Chagunava <145435217+Rostom-Chagunava@users.noreply.github.com>
2023-10-14 16:27:30 +0200

    Add files via upload

---
+++ day7/homework/lessen7.py
@@ -0,0 +1,51 @@
+
+# sum = 0
+# x = 10
+# while x > 0:
+#   sum += x
+#   x -= 1
+
+# print(sum)
+
+# i = 0
+# while True:
+#   print(i)
+#   i = i + 1
+#   if i >= 5:
+#     print("Breaking")
+#     break
+
+# print("Finished")
+# i = 5
+
+# while True:
+
+#   print(i)
+
+#   i = i - 1
+
+#   if i <= 2:
+
+#     break
+# i = 0
+# while i<5:
+#   i += 1
+#   if i==3:
+#     print("Skipping 3")
+#     continue
+#   print(i)
+
+
+
+
+#1 
+List_leaders=["მოლოდინი","ჯანეზაშვილი","ტყეშელაშვილი","ფილიშვილი"]
+
+print(List_leaders[2])
+#2
+squad_members=["როსტომი","მერაბი","გიო","გიგი"]
+print(squad_members[1:3])
+#3
+list_Frui=["appel","pear","plum","potatoes","peach"]
+list_Frui[3]="mango"
+print(list_Frui)
