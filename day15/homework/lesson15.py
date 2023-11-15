e96ced3cb3c119acbbc66b14d8048909fd2c004c
Rostom-Chagunava <145435217+Rostom-Chagunava@users.noreply.github.com>
2023-11-15 22:10:54 +0100

    Add files via upload

---
+++ day15/homework/lesson15.py
@@ -0,0 +1,33 @@
+
+
+# 1 დავალება 
+def square_sum(numbers):
+    sum = 0
+    for i in numbers:
+        sum += i*i
+    return sum    
+   
+
+# 2 davaleba 
+def no_space(x):
+    y = ""
+    for i in x:
+        if i  !=  " " :
+            y += i
+    return y 
+
+
+# 3 davaleba
+def liter (times):
+    return int(times / 2)
+print (liter(25))
+
+
+
+
+  
+
+                 
+
+
+        
